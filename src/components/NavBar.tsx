089c7adcbfcdbb928492d75e5c85bb99f457c875
saleempirwani <msr.pirwani@gmail.com>
2023-11-11 15:11:47 +0500

    navbar done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { NAV_LINKS } from "@/data/mock";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { twMerge } from "tailwind-merge";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const pathname = usePathname();
+
+  console.log(pathname);
+
+  return (
+    <nav className="flex space-x-6 mb-5 px-5 h-14 border-b items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {NAV_LINKS.map((navLink, index) => (
+          <li
+            key={index}
+            className={twMerge(
+              "text-zinc-500 hover:text-zinc-800 transition-colors",
+              pathname === navLink.link && "text-zinc-900"
+            )}
+          >
+            <Link href={navLink.link}>{navLink.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
---
+++ src/data/mock.ts
@@ -0,0 +1,4 @@
+export const NAV_LINKS = [
+  { label: "Dashboard", link: "/" },
+  { label: "Issues", link: "/issues" },
+];
