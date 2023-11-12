e04d917d00ccaf5aa5843f541759c90e52c6e4f4
saleempirwani <msr.pirwani@gmail.com>
2023-11-12 17:02:08 +0500

    func added

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -4,13 +4,52 @@ import IssueBadge from "@/components/IssueBadge";
 import SelectMenu from "@/components/SelectMenu";
 import { ISSUES } from "@/data/mock";
 import { Status } from "@/types";
-import { Button, Flex, Heading, ScrollArea, Text } from "@radix-ui/themes";
+import {
+  AlertDialog,
+  Button,
+  Flex,
+  Heading,
+  ScrollArea,
+  Text,
+} from "@radix-ui/themes";
 import { FaTrash } from "react-icons/fa";
 import { FaPenToSquare } from "react-icons/fa6";
 import { marked } from "marked";
+import Link from "next/link";
 
 const IssuePage = () => {
   const issue = ISSUES[0];
+  const id = "1";
+
+  const renderDelete = () => (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" className="cursor-pointer">
+          <FaTrash />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? You want to delete this issue.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray" className="cursor-pointer">
+              No
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" className="cursor-pointer">
+              Yes
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 
   return (
     <Flex className="p-5" gap="9">
@@ -35,14 +74,11 @@ const IssuePage = () => {
       <div className="flex-[0.2] flex flex-col gap-y-5">
         <SelectMenu />
         <div className="flex flex-col gap-y-2">
-          <Button className="bg-green-500 cursor-pointer">
+          <Button color="green" className="cursor-pointer">
             <FaPenToSquare />
-            Edit Issue
+            <Link href={`/issues/edit/${id}`}>Edit Issue</Link>
           </Button>
-          <Button className="bg-red-500 cursor-pointer">
-            <FaTrash />
-            Delete Issue
-          </Button>
+          {renderDelete()}
         </div>
       </div>
     </Flex>
