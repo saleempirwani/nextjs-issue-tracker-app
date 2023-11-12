a96c4671141e303368ab9a8e9ebcb8d05303c0b3
saleempirwani <msr.pirwani@gmail.com>
2023-11-12 14:10:02 +0500

    cursor updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,6 +18,7 @@ const NavBar = () => {
         <Avatar
           src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
           fallback="A"
+          className="cursor-pointer"
         />
       </Popover.Trigger>
       <Popover.Content>
