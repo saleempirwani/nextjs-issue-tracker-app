72dc211e6d3ff2818acac7ef567e73e2f315d464
saleempirwani <msr.pirwani@gmail.com>
2023-11-12 12:39:01 +0500

    navbar updated

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 
 const IssuesPage = () => {
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,58 @@
 "use client";
 
 import { NAV_LINKS } from "@/data/mock";
+import { Avatar, Popover, Text } from "@radix-ui/themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import { twMerge } from "tailwind-merge";
 
 const NavBar = () => {
   const pathname = usePathname();
 
-  console.log(pathname);
+  const onLogout = () => {};
+
+  const renderAvatar = () => (
+    <Popover.Root>
+      <Popover.Trigger>
+        <Avatar
+          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+          fallback="A"
+        />
+      </Popover.Trigger>
+      <Popover.Content>
+        <Text as="p">msr.piwani.dev@gmail.com</Text>
+        <Popover.Close>
+          <Text color="red" className="cursor-pointer" onClick={onLogout}>
+            Logout
+          </Text>
+        </Popover.Close>
+      </Popover.Content>
+    </Popover.Root>
+  );
 
   return (
-    <nav className="flex space-x-6 mb-5 px-5 h-14 border-b items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {NAV_LINKS.map((navLink, index) => (
-          <li
-            key={index}
-            className={twMerge(
-              "text-zinc-500 hover:text-zinc-800 transition-colors",
-              pathname === navLink.link && "text-zinc-900"
-            )}
-          >
-            <Link href={navLink.link}>{navLink.label}</Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="flex items-center justify-between mb-5 px-5 h-14 border-b ">
+      <div className="flex items-center space-x-6">
+        <Link href="/">
+          <AiFillBug />
+        </Link>
+
+        <ul className="flex space-x-6">
+          {NAV_LINKS.map((navLink, index) => (
+            <li
+              key={index}
+              className={twMerge(
+                "text-zinc-500 hover:text-zinc-800 transition-colors",
+                pathname === navLink.link && "text-zinc-900"
+              )}
+            >
+              <Link href={navLink.link}>{navLink.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {renderAvatar()}
     </nav>
   );
 };
