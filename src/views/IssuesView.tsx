90d4c1617c6a5af054614279dcfbdb9878d563aa
saleempirwani <msr.pirwani@gmail.com>
2023-11-12 17:41:18 +0500

    issue solved

--- src/views/IssuesView.tsx
+++ src/views/IssuesView.tsx
@@ -1,6 +1,7 @@
 import IssuesTable from "@/components/IssuesTable";
 import Pagination from "@/components/Pagination";
 import SelectMenu from "@/components/SelectMenu";
+import { STATUS_SELECT_MENU } from "@/data/appData";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
@@ -9,7 +10,7 @@ const IssuesView = () => {
   return (
     <div className="p-5">
       <div className="flex items-center justify-between mb-5">
-        <SelectMenu />
+        <SelectMenu data={STATUS_SELECT_MENU} defaultValue="all" />
         <Button>
           <Link href="/issues/new">New Issue</Link>
         </Button>
